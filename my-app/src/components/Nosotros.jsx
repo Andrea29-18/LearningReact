b24714bfe207d7ea27d84a07ef88159604973bdc
Andrea29-18 <aavp0316@gmail.com>
2024-08-04 18:43:43 -0600

    Eventos

--- my-app/src/components/Nosotros.jsx
+++ my-app/src/components/Nosotros.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Nosotros = () => {
     return (
-        <div>
-            <h1>Nosotros</h1>
-            Este es el componente "Nosotros"
+        <div className='container'>
+            <h1 className='main-title'>Nosotros</h1>
+            <p>Este es el componente "Nosotros"</p>
         </div>
     )
 }
